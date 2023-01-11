7c3ba09b09f1dc0104b37ee1d60cc657506234b2
Tianon Gravi <admwiggin@gmail.com>
2023-01-11 12:18:59 -0800

    Fix support for unstable/sid by supporting/preferring `debian.sources` over `sources.list`

--- download.sh
+++ download.sh
@@ -23,14 +23,35 @@ for suite in "${suites[@]}"; do
 	target="suites/$suite"
 	mkdir -p "$target"
 
-	if ! wget -qO "$target/sources.list" "https://github.com/debuerreotype/docker-debian-artifacts/raw/$commit/$suite/rootfs.sources-list-snapshot"; then
-		echo "Skipping '$suite' (sources.list download failed)"
+	foundFile=
+	for file in \
+		'rootfs.debian-sources-snapshot=debian.sources' \
+		'rootfs.sources-list-snapshot=sources.list' \
+	; do
+		remoteFile="${file%%=*}"
+		localFile="${file#$remoteFile=}"
+		if ! wget -qO "$target/$localFile" "https://github.com/debuerreotype/docker-debian-artifacts/raw/$commit/$suite/$remoteFile"; then
+			rm -rf "$target/$localFile"
+			continue
+		fi
+		foundFile="$localFile"
+		break
+	done
+	if [ -z "$foundFile" ]; then
+		echo "Skipping '$suite' (debian.sources / sources.list download failed)"
 		continue
+	fi
+
+	if [ "$foundFile" = 'sources.list' ]; then
+		targetDir='/etc/apt'
+	else
+		targetDir='/etc/apt/sources.list.d'
 	fi
 
 	cat > "$target/Dockerfile" <<-EODF
 		FROM debian:$suite-$serial
 		RUN echo 'Acquire::Check-Valid-Until "false";' > /etc/apt/apt.conf.d/docker-snapshot.conf
-		COPY sources.list /etc/apt/
+		RUN [ -s "$targetDir/$foundFile" ]
+		COPY $foundFile $targetDir/
 	EODF
 done
